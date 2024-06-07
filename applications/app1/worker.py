31ee9fa62233d0f1d1376054504a85c77b2fe310
Carmen Elisa <carmen@anota.ai>
2024-06-07 12:18:47 -0300

    feat: change apps

---
+++ applications/app1/worker.py
@@ -0,0 +1,8 @@
+def calcular_imc(peso, altura):
+    return peso / (altura ** 2)
+
+peso = float(input("Digite seu peso em kg: "))
+altura = float(input("Digite sua altura em metros: "))
+
+imc = calcular_imc(peso, altura)
+print("Seu IMC é:", imc)
